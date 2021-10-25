45fd7d5b85b932d75e94644f299b7834bd44a200
connorwarme <connor.warme@gmail.com>
2021-10-24 20:16:12 -0600

    small updates, learning about button with js function

--- sketchy.js
+++ sketchy.js
@@ -3,13 +3,22 @@ for (i=0; i<256; i++) {
     d += `<div class="box"></div>`;
 }
 const container = document.querySelector('div.container');
-const divs = document.querySelectorAll('.box');
+console.log(d);
 container.innerHTML = d;
+document.getElementsByClassName('box').appendChild('div')
+const divs = document.getElementsByClassName('box');
 
 container.addEventListener("mousemove", (e) => {
     changeColor(e)
 });
 
 function changeColor(e) {
-    e.target.style.background = 'black';
+    e.target.classList.add('active');
 };
+
+let restart = document.querySelector('button.reset');
+restart.addEventListener('click', reset());
+
+function reset() {
+    divs.classList.remove('active');
+}
