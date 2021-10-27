a31f5a37fa6971807dba616ae5de5708cec96f48
connorwarme <connor.warme@gmail.com>
2021-10-26 21:21:47 -0600

    added a slider range, fixed grid size but options from 16x16 to 64x64, working on other colors

--- sketchy.js
+++ sketchy.js
@@ -1,24 +1,62 @@
-let d = "";
-for (i=0; i<256; i++) {
-    d += `<div class="box"></div>`;
-}
 const container = document.querySelector('div.container');
-console.log(d);
-container.innerHTML = d;
-document.getElementsByClassName('box').appendChild('div')
-const divs = document.getElementsByClassName('box');
+console.log(typeof(container));
+const slider = document.getElementById('gridRange');
+const output = document.getElementById('output');
+
+
+slider.oninput = function () {
+    output.textContent = `Grid size: ${this.value} x ${this.value}`;
+    console.log(this.value);
+    const newGridSize = this.value;
+    makeGrid(newGridSize);
+}
+const gridSize = Number(slider.value);
+
+function clearGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+}
+
+function makeGrid(input) {
+    clearGrid(container);
+    let gridWidth = (container.offsetWidth / input);
+container.style.cssText = `grid-template-columns: repeat(${input}, minmax(${gridWidth}px, auto));
+    grid-template-rows: repeat(${input}, minmax(${gridWidth}px, auto));`;
+
+    for (i=0; i<(input ** 2); i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    container.appendChild(div);
+    };
+}
+
+makeGrid(gridSize); 
+
+const group = document.getElementsByClassName('box');
+const divs = Array.from(group);
+console.log(divs);
 
 container.addEventListener("mousemove", (e) => {
-    changeColor(e)
+    colorBlack(e)
 });
 
-function changeColor(e) {
+function colorBlack(e) {
     e.target.classList.add('active');
 };
 
 let restart = document.querySelector('button.reset');
-restart.addEventListener('click', reset());
+restart.addEventListener('click', e => {
+    clearGrid(container);
+    makeGrid(Number(slider.value));
+    console.log(e);
+});
 
-function reset() {
-    divs.classList.remove('active');
-}
+let rainbow = document.querySelector('button.rainbow');
+rainbow.addEventListener('click', e => {
+    const active = Array.from(document.querySelectorAll('div.active'));
+    console.log(active);
+    container.appendChild('.active');
+    active.style.cssText = "background-color: blue;";
+    
+});
